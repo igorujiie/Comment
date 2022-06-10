69a88b11f365fb387c76c2897c5ca91d5fe6ce1c
igor ujiie <igorkazuhico@hotmail.com>
2022-06-09 22:32:40 -0300

    feito alteracao

--- src/main/java/com/ifsp/comenteSobre/controller/ComentesobreController.java
+++ src/main/java/com/ifsp/comenteSobre/controller/ComentesobreController.java
@@ -5,6 +5,7 @@ import com.ifsp.comenteSobre.model.Post;
 import com.ifsp.comenteSobre.service.ComentesobreService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,10 +19,18 @@ public class ComentesobreController {
     ComentesobreService comentesobreService;
 
     @RequestMapping(value = "/posts", method = RequestMethod.GET)
-    public ModelAndView getPost(){
+    public ModelAndView getPosts(){
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = comentesobreService.findAll();
         mv.addObject("posts",posts);
+        return mv;
+    }
+
+    @RequestMapping(value="/posts/{id}", method=RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id") long id){
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = comentesobreService.findById(id);
+        mv.addObject("post", post);
         return mv;
     }
 
